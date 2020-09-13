b713136d566efea55d3d3f7a884e0369b6d4619d
xp <>
2020-09-13 01:43:54 -0400

    we now have a good wrapper

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,20 @@
-from flask import Blueprint
-def create_api():
-    api = Blueprint(name=api, url_prefix="api")
+from flask import Blueprint, current_app, request
 
-    return api
+api = Blueprint(name='api', url_prefix="/api", import_name='api')
+
+@api.route('/test')
+def api_test():
+    return 'test'
+
+@api.route('/create_radare_session', methods=["POST"])
+def api_create_radare_session():
+    dir = request.values['dir']
+    id = current_app.sm.create_radare_session(dir)
+    return {"session_id": id}
+
+@api.route('/execute', methods=["POST"])
+def api_execute():
+    id = request.values['id']
+    cmd = request.values['command']
+    out = current_app.sm[id].execute(cmd)
+    return {"output": out}
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,14 +2,36 @@ from flask import Flask
 
 def create_app(config=None):
     app = Flask(__name__)
+    import os
     if config:
         app.config.from_pyfile(config)
     else:
-        pass #TODO: create default config
+        #default config
+        app.config['RADARE_INSTALL_DIR'] = "../radare2-install"
+        app.config['REACT_BUILD_DIR'] = ""
 
-    try:
+
+        app.config['SECRET_KEY'] = os.urandom(32).hex()
+
+    try: # registering session manager
+        from app import r2pipewrapper
+        app.sm = r2pipewrapper.sm
+        app.logger.info("SessionManager registered.")
+    except Exception as e:
+        app.logger.error(f"SessionManager could not register: {e}")
+
+
+    try: # registering errorhandlers
+        from app import errorhandlers
+        errorhandlers.bind_error_handlers(app)
+        app.logger.info("Errorhandlers registered.")
+    except Exception as e:
+        app.logger.error(f"Errorhandlers could not register: {e}")
+
+    try:  # registering api
+        # app.logger.info(f"Trying to register API BP. CWDIR: {os.getcwd()}")
         from app import api
-        app.register_blueprint(api)
+        app.register_blueprint(api.api)
         app.logger.info("API blueprint registered.")
     except Exception as e:
         app.logger.error(f"API blueprint could not register: {e}")
